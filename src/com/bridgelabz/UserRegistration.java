eed842203488f75159d8ff134cd6df37bf280cf5
succhi111 <suchendramishra396@gmail.com>
2022-09-23 19:12:30 +0530

    Enter a valid last Name

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -3,6 +3,7 @@ package com.bridgelabz;
 public class UserRegistration {
     public static void main(String[] args) {
         System.out.println(firstName("Suchendra"));
+        System.out.println(lastName("Mishra"));
     }
 
     /*
@@ -11,6 +12,13 @@ public class UserRegistration {
     public static boolean firstName(String firstName) {
 
         return firstName.matches("[A-Z][a-z]{3,}");
+    }
+    /*
+    validate last name with 1st letter capital and at least 3 words
+    */
+    public static boolean lastName(String lastName) {
+
+        return lastName.matches("[A-Z][a-z]{3,}");
     }
 }
 
